fff2b7cc3af5188a0920e90b79815aaf350ec96d
Renata Saliкhzianova <r.salikhzyanova@innopolis.university>
2025-06-15 06:35:02 +0000

    Edit models.py

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ class Syllabus(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="syllabuses")
     messages = relationship("Message", back_populates="syllabus")
+    topics = relationship("Topic", back_populates="syllabus")
 
 class Message(Base):
     __tablename__ = "messages"
@@ -31,3 +32,32 @@ class Message(Base):
     syllabus_id = Column(Integer, ForeignKey("syllabuses.id"))
     user = relationship("User", back_populates="messages")
     syllabus = relationship("Syllabus", back_populates="messages")
+
+
+class Topic(Base):
+    __tablename__ = "topics"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    syllabus_id = Column(Integer, ForeignKey("syllabuses.id"))
+    syllabus = relationship("Syllabus", back_populates="topics")
+    tasks = relationship("Task", back_populates="topic")
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True)
+    description = Column(Text)
+    difficulty = Column(String)
+    topic_id = Column(Integer, ForeignKey("topics.id"))
+    topic = relationship("Topic", back_populates="tasks")
+    solutions = relationship("Solution", back_populates="task")
+
+class Solution(Base):
+    __tablename__ = "solutions"
+    id = Column(Integer, primary_key=True)
+    content = Column(Text)
+    is_correct = Column(Integer)  # 1 или 0
+    submitted_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    task_id = Column(Integer, ForeignKey("tasks.id"))
+    task = relationship("Task", back_populates="solutions")
+
