6eb23a5dabf3fc35f90b628ee88bf8de9b65e36e
kriss <97190065+Kriss221@users.noreply.github.com>
2025-06-23 05:24:20 +0300

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -13,7 +13,7 @@ export default function LoginPage({ onClose }) {
         }
 
         try {
-            const res = await fetch('http://127.0.0.1:8000/login', {
+            const res = await fetch('studybuddy-team-production.up.railway.app', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
