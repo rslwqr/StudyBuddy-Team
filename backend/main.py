f39796b9f1d8265a1c1906f3b2d130702c27b453
kriss <97190065+Kriss221@users.noreply.github.com>
2025-06-23 05:41:37 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,9 +31,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174", "http://localhost:5174"
+    allow_origins=[https://ornate-quokka-a43c11.netlify.app
     ],
     allow_credentials=True,
     allow_methods=["*"],
